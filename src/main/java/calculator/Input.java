5557d61970d1c8055cae934527942db48950598c
SeonWu Kim <sunwoo005@naver.com>
2025-10-17 23:58:43 +0900

    feat(Input): 문자 입력받기 구현

---
+++ src/main/java/calculator/Input.java
@@ -0,0 +1,12 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+    private final String INPUT_START_GUIDE = "덧셈할 문자열을 입력해 주세요.";
+
+    public String readInput() {
+        System.out.println(INPUT_START_GUIDE);
+        return Console.readLine();
+    }
+}
