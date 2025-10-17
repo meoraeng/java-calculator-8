a6ba29b63a0eda70eb4038b7b8d0c3bca18e7974
SeonWu Kim <sunwoo005@naver.com>
2025-10-18 01:41:58 +0900

    feat(SepeartorDetector): 입력받은 문자열 복제해서 관리

---
+++ src/main/java/calculator/SeperatorDetector.java
@@ -0,0 +1,12 @@
+package calculator;
+
+import java.util.ArrayList;
+
+public class SeperatorDetector {
+    private String inputString;
+    private ArrayList<String> seperators;
+
+    public SeperatorDetector(String str) {
+        this.inputString = str;
+    }
+}
