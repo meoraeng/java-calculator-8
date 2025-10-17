52bcc0c0a96f66962e5f74113c0255edfdd5b98b
SeonWu Kim <sunwoo005@naver.com>
2025-10-18 01:46:14 +0900

    feat(SepeartorDetector): 문자열에 커스텀 구분자 문법이 있는 경우 뒤쪽 문자열과 분리하고 구분자 추가

--- src/main/java/calculator/SeperatorDetector.java
+++ src/main/java/calculator/SeperatorDetector.java
@@ -9,4 +9,20 @@ public class SeperatorDetector {
     public SeperatorDetector(String str) {
         this.inputString = str;
     }
+
+    private void stringUpdate(String str){
+        this.inputString = str;
+    }
+
+    public String findCustomSeperator(){
+        int start = inputString.indexOf("//");
+        int end = inputString.indexOf("\n, start+2");
+        boolean ok = start == 0 && end != -1;
+
+        if(!ok) {
+            return "";
+        }
+        stringUpdate(inputString.substring(end+2));
+        return inputString.substring(start+2, end);
+    }
 }
