71ef891d7d925127a384eade8f38e18cb465081b
SeonWu Kim <sunwoo005@naver.com>
2025-10-19 00:40:11 +0900

    feat(Controller): 음수값, 유효하지 않은 값 입력받는 경우 예외처리

--- src/main/java/calculator/Controller.java
+++ src/main/java/calculator/Controller.java
@@ -43,10 +43,24 @@ public class Controller {
     }
 
     private void splitStringToNumber(String str, int seperatorIdx) {
-        int number = Integer.parseInt(str.substring(0,seperatorIdx));
-        String remainString = str.substring(seperatorIdx+1);
-        numberList.add(number);  // 앞쪽값은 numberList.add
-        this.inputString = remainString; // 뒤쪽값을 새 문자열로 갱신
+        String numberToken = str.substring(0, seperatorIdx);
+
+        try {
+            int number = Integer.parseInt(numberToken);
+
+            if (number < 0) { // validaton 분리
+                throw new IllegalArgumentException(
+                        "음수값은 허용되지 않습니다.: " + numberToken
+                );
+            }
+            String remainString = str.substring(seperatorIdx + 1);
+            numberList.add(number);  // 앞쪽값은 numberList.add
+            this.inputString = remainString; // 뒤쪽값을 새 문자열로 갱신
+         } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(
+                    "정수 범위를 초과했거나 유효하지 않은 값이 포함되어 있습니다.: " + numberToken
+            );
+        }
     }
     private int calculateNumber(ArrayList<Integer> numberList){
         int sum = 0;
